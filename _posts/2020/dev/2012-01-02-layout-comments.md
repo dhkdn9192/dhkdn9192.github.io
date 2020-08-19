0ac934a97cb0acb662dea5d85c8762aaa984e3f0
김동혁 <dhkdn9192@naver.com>
2020-08-20 02:07:26 +0900

    theme

---
+++ _posts/2020/dev/2012-01-02-layout-comments.md
@@ -0,0 +1,13 @@
+---
+title: "Layout: Comments Enabled"
+layout: single
+comments: true
+categories:
+  - Layout
+  - Uncategorized
+tags:
+  - comments
+  - layout
+---
+
+This post should display comments if a `provider` is enabled.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Layout: Comments Enabled",
+        "excerpt":"This post should display comments if a provider is enabled.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["comments","layout"],
+        "url": "http://localhost:4000/layout/uncategorized/layout-comments/",
+        "teaser": null
+      }]
